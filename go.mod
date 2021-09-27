9c5055c5cc43042f149948995cfa95aef3b87506
Mehul Joshi <joshimehulv@gmail.com>
2021-09-26 22:59:13 -0700

    finished login route

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Bean/m
 go 1.17
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/lib/pq v1.10.3 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 )
--- src/server/auth_handlers.go
+++ src/server/auth_handlers.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var jwtKey = []byte("secret")
 
 // This strcut models the structure of a user in both the request body
 // and the DB
 type Credentials struct {
 	Password string `json:"password", db:"password"`
 	Username string `json:"username", db:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
@@ -32,3 +42,60 @@ func signup(w http.ResponseWriter, r *http.Request) {
 }
 
 //todo: add a session based signin route with jwt-based-auth
+func signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res := db.QueryRow(`select password from users where username = $1`, creds.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	storedCreds := &Credentials{}
+	err = res.Scan(&storedCreds.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	//otherwise we are authorized
+	//so create a new jwt token
+	// token := jwt.NewWithClaims(jwt.SigningMethodsHS256, claims)
+	experationTime := time.Now().Add(5 * time.Minute)
+
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: experationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: experationTime,
+	})
+}
