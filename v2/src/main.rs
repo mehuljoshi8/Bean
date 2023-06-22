3d729b0e8c59651d6882b4ffdaafb1f2d71d8bfc
Mehul Joshi <mehuljoshi@Mehuls-MacBook-Pro.local>
2023-06-22 14:00:15 -0700

    completed basic tcp server on port bean

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // 2326 is bean spelled out :)
+    let listener = TcpListener::bind("127.0.0.1:2326").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connection established!");
+    }
 }
